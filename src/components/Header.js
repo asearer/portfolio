028adc8d8c18aeed109f79b968e5dc544203323d
asearer <amsearer84@gmail.com>
2024-08-06 16:16:23 -0400

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   src/components/Header.css 	modified:   src/components/Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
-const Header = () => (
-  <header className="neumorphic-card">
-    <nav>
-      <h1>asearerdev</h1>
-      <ul>
-        <li><Link className="neumorphic-button" to="/">Home</Link></li>
-        <li><Link className="neumorphic-button" to="/about">About</Link></li>
-        <li><Link className="neumorphic-button" to="/skills">Skills</Link></li>
-        <li><Link className="neumorphic-button" to="/projects">Projects</Link></li>
-        <li><Link className="neumorphic-button" to="/blog">Blog</Link></li>
-        <li><Link className="neumorphic-button" to="/admin">Admin</Link></li>
-      </ul>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleLoginClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <header className="neumorphic-card">
+      <nav>
+        <h1>asearerdev</h1>
+        <ul>
+          <li><Link className="neumorphic-button" to="/">Home</Link></li>
+          <li><Link className="neumorphic-button" to="/about">About</Link></li>
+          <li><Link className="neumorphic-button" to="/skills">Skills</Link></li>
+          <li><Link className="neumorphic-button" to="/projects">Projects</Link></li>
+          {/* <li><Link className="neumorphic-button" to="/blog">Blog</Link></li> */}
+          {/* <li><Link className="neumorphic-button" to="/contact">Contact</Link></li> */}
+          {/* <li><button className="neumorphic-button" onClick={handleLoginClick}>Admin</button></li> */}
+        </ul>
+      </nav>
+
+      {/* Login Modal */}
+      {isModalOpen && (
+        <div className="login-modal">
+          <h2>Login</h2>
+          <form>
+            <label>
+              Username
+              <input type="text" name="username" />
+            </label>
+            <label>
+              Password
+              <input type="password" name="password" />
+            </label>
+            <button type="submit">Login</button>
+            <button type="button" onClick={handleCloseModal}>Close</button>
+          </form>
+        </div>
+      )}
+    </header>
+  );
+};
 
 export default Header;
+
+
 
 
