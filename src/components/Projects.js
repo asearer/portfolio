af8774975721120ef38d243477a6d514250a1772
asearer <amsearer84@gmail.com>
2024-08-14 17:06:23 -0400

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   src/components/Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,9 +16,65 @@ const Projects = () => (
     <h2>Projects</h2>
     <h2 className="under-construction">More coming soon...Feel free to browse my GitHub or CodePen!</h2>
     {/* <div className="projects-grid">
-      // ProjectCard components here...
+      <ProjectCard
+        title="Project 1"
+        description="Description of Project 1"
+        techStack="JavaScript, React"
+        liveDemo="https://example.com"
+        sourceCode="https://github.com/example/repo"
+      />
+      <ProjectCard
+        title="Project 2"
+        description="Description of Project 2"
+        techStack="HTML, CSS, JavaScript"
+        liveDemo="https://example.com"
+        sourceCode="https://github.com/example/repo"
+      />
+      <ProjectCard
+        title="Project 3"
+        description="Description of Project 3"
+        techStack="Python, Django"
+        liveDemo="https://example.com"
+        sourceCode="https://github.com/example/repo"
+      />
+      <ProjectCard
+        title="Project 4"
+        description="Description of Project 4"
+        techStack="Node.js, Express"
+        liveDemo="https://example.com"
+        sourceCode="https://github.com/example/repo"
+      />
+      <ProjectCard
+        title="Project 5"
+        description="Description of Project 5"
+        techStack="Python, Flask" 
+        liveDemo="https://example.com"
+        sourceCode="https://github.com/example/repo"
+      />
+      <ProjectCard
+        title="Project 6"
+        description="Description of Project 6"
+        techStack="Vue.js"
+        liveDemo="https://example.com"
+        sourceCode="https://github.com/example/repo"
+      />
+      <ProjectCard
+        title="Project 7"
+        description="Description of Project 7"
+        techStack="Python, SQLAlchemy, PostgreSQL"
+        liveDemo="https://example.com"
+        sourceCode="https://github.com/example/repo"
+      />
+      <ProjectCard
+        title="Project 8"
+        description="Description of Project 8"
+        techStack="React, MongoDB"
+        liveDemo="https://example.com"
+        sourceCode="https://github.com/example/repo"
+      />
     </div> */}
   </section>
 );
 
 export default Projects;
+
