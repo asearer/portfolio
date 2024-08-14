dc5eb605ed8954c5ed23de9c711d248066ea304a
asearer <amsearer84@gmail.com>
2024-08-14 16:59:07 -0400

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   src/components/Projects.css 	modified:   src/components/Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,69 +14,11 @@ const ProjectCard = ({ title, description, techStack, liveDemo, sourceCode }) =>
 const Projects = () => (
   <section id="projects">
     <h2>Projects</h2>
-    <h2>More coming soon...Feel free to browse my GitHub or CodePen!</h2>
+    <h2 className="under-construction">More coming soon...Feel free to browse my GitHub or CodePen!</h2>
     {/* <div className="projects-grid">
-      <ProjectCard
-        title="Project 1"
-        description="Description of Project 1"
-        techStack="JavaScript, React"
-        liveDemo="https://example.com"
-        sourceCode="https://github.com/example/repo"
-      />
-      <ProjectCard
-        title="Project 2"
-        description="Description of Project 2"
-        techStack="HTML, CSS, JavaScript"
-        liveDemo="https://example.com"
-        sourceCode="https://github.com/example/repo"
-      />
-      <ProjectCard
-        title="Project 3"
-        description="Description of Project 3"
-        techStack="Python, Django"
-        liveDemo="https://example.com"
-        sourceCode="https://github.com/example/repo"
-      />
-      <ProjectCard
-        title="Project 4"
-        description="Description of Project 4"
-        techStack="Node.js, Express"
-        liveDemo="https://example.com"
-        sourceCode="https://github.com/example/repo"
-      />
-      <ProjectCard
-  title="Project 5"
-  description="Description of Project 5"
-  techStack="Python, Flask" 
-  liveDemo="https://example.com"
-  sourceCode="https://github.com/example/repo"
-/>
-
-      <ProjectCard
-        title="Project 6"
-        description="Description of Project 6"
-        techStack="Vue.js"
-        liveDemo="https://example.com"
-        sourceCode="https://github.com/example/repo"
-      />
-      <ProjectCard
-        title="Project 7"
-        description="Description of Project 7"
-        techStack="Python, SQLAlchemy, PostgreSQL"
-        liveDemo="https://example.com"
-        sourceCode="https://github.com/example/repo"
-      />
-      <ProjectCard
-        title="Project 8"
-        description="Description of Project 8"
-        techStack="React, MongoDB"
-        liveDemo="https://example.com"
-        sourceCode="https://github.com/example/repo"
-      />
+      // ProjectCard components here...
     </div> */}
   </section>
 );
 
 export default Projects;
-
-
