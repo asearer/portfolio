196a93ec25e15849b7a08df8e579b4a7bab5f1bb
asearer <amsearer84@gmail.com>
2024-08-16 10:39:57 -0400

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ import HeroSection from './components/HeroSection';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import Blog from './components/Blog';
-import Contact from './components/Contact';
-import AdminPage from './components/AdminPage'; 
+
+ 
 import './App.css'; // Import global styles
 
 // Import FontAwesome CSS for icons
@@ -21,9 +20,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/admin" element={<AdminPage />} />
+        
+        
+        
       </Routes>
     </Layout>
   );
