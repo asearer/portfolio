dad4e7211fff9ed786337320cd357ba1084a2f47
asearer <amsearer84@gmail.com>
2024-08-17 11:26:40 -0400

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   src/components/HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,7 +4,7 @@ import './HeroSection.css';
 // Import FontAwesome icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
-import { faFacebook, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faLinkedin, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 
 
 const HeroSection = () => {
@@ -25,7 +25,7 @@ const HeroSection = () => {
       {showContact && (
         <div className="contact-info">
           <p><FontAwesomeIcon icon={faLinkedin} /> LinkedIn: <a href="https://www.linkedin.com/in/alonzasearer/" target="_blank" rel="noopener noreferrer">Alonza Searer</a></p>
-          <p><FontAwesomeIcon icon={faTwitter} /> Twitter/X: <a href="https://x.com/AlonzaSearer" target="_blank" rel="noopener noreferrer">Alonza Searer</a></p>
+          <p><FontAwesomeIcon icon={faXTwitter} /> : <a href="https://x.com/AlonzaSearer" target="_blank" rel="noopener noreferrer">Alonza Searer</a></p>
         <p><FontAwesomeIcon icon={faEnvelope} /> Email: <a href="mailto:asearerdev@gmail.com">asearerdev@gmail.com</a></p>
         <p><FontAwesomeIcon icon={faFacebook} /> Facebook: <a href="https://www.facebook.com/ASearerDev" target="_blank" rel="noopener noreferrer">ASearerDev</a></p>
         {/* <p><FontAwesomeIcon icon={faPhone} /> Phone: (123) 456-7890</p> */}
